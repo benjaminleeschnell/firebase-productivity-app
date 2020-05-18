5aa102a46506dbe54e983bcb64d6f9efea6bc39a
Ben Schnell <benschnell@southern.edu>
2020-05-18 13:05:35 -0400

    a bunch

--- main.js
+++ main.js
@@ -18,6 +18,7 @@ form.addEventListener('submit', function (e) {
   e.preventDefault();
   addTask(box);
   create_unfinished_task();
+  box.value = '';
 });
 
 function addTask(input) {
@@ -25,11 +26,12 @@ function addTask(input) {
     const key = firebase.database().ref().child('unfinished_key').push().key;
     var task = {
       title: input.value,
+      state: false,
       key: key,
     };
 
     var updates = {};
-    updates['/unfinished_task/' + key] = task;
+    updates['/tasks/' + key] = task;
     firebase.database().ref().update(updates);
   }
 }
@@ -41,7 +43,7 @@ function create_unfinished_task() {
   task_array = [];
   firebase
     .database()
-    .ref('unfinished_task')
+    .ref('tasks')
     .once('value', function (snapshot) {
       snapshot.forEach(function (childSnapshot) {
         var childKey = childSnapshot.key;
@@ -50,7 +52,8 @@ function create_unfinished_task() {
       });
       for (var i, i = 0; i < task_array.length; i++) {
         task_key = task_array[i][0];
-        task_title = task_array[i][1];
+        state = task_array[i][1];
+        task_title = task_array[i][2];
 
         task_container = document.createElement('div');
         task_container.setAttribute('class', 'task_container');
@@ -59,6 +62,11 @@ function create_unfinished_task() {
         task_data = document.createElement('div');
         task_data.setAttribute('class', 'task_data');
 
+        checkbox = document.createElement('input');
+        checkbox.setAttribute('class', 'done');
+        checkbox.type = 'checkbox';
+        checkbox.checked = state;
+
         title = document.createElement('p');
         title.setAttribute('class', 'task_title');
         title.setAttribute('contenteditable', false);
@@ -66,12 +74,6 @@ function create_unfinished_task() {
 
         task_tool = document.createElement('div');
         task_tool.setAttribute('class', 'task_tool');
-
-        task_done_button = document.createElement('button');
-        task_done_button.setAttribute('class', 'task_done_button');
-
-        fa_done = document.createElement('i');
-        fa_done.setAttribute('class', 'fa fa-check');
 
         task_edit_button = document.createElement('button');
         task_edit_button.setAttribute('class', 'task_edit_button');
@@ -85,11 +87,12 @@ function create_unfinished_task() {
 
         unfin_container.append(task_container);
         task_container.append(task_data);
+        task_data.append(checkbox);
         task_data.append(title);
         task_container.append(task_tool);
-
-        task_tool.append(task_done_button);
-        task_done_button.append(fa_done);
+        if (state === true) {
+          task_container.classList.add('finished');
+        }
 
         task_tool.append(task_edit_button);
         task_edit_button.append(fa_edit);
@@ -99,82 +102,138 @@ function create_unfinished_task() {
       }
 
       taskDone(unfin_container);
-      taskEdit(task_container);
-      taskDelete(task_container);
-    });
+      taskEdit(unfin_container);
+      taskDelete(unfin_container);
+    })
+    .then(() => {
+      return firebase
+        .database()
+        .ref('tasks')
+        .once('value', function (snapshot) {
+          snapshot.forEach(function (childSnapshot) {
+            var childKey = childSnapshot.key;
+            var childData = childSnapshot.val();
+            task_array.push(Object.values(childData));
+          });
+        });
+      // taskReorder(unfin_container);
+    })
+    .catch(console.log('error'));
+}
+
+function taskReorder(unfin_container) {
+  let finished = unfin_container.getElementsByClassName('.finished');
+  for (let i = 0; i < finished.length; i++) {
+    console.log(finished[i]);
+    finished[i].removeChild();
+    unfin_container.append(finished[i]);
+  }
 }
 
 function taskDone(unfin_container) {
-  let edit_button = unfin_container.querySelectorAll('.task_done_button');
+  let done_button = unfin_container.querySelectorAll('.done');
+  for (let i = 0; i < done_button.length; i++) {
+    done_button[i].addEventListener('change', function (e) {
+      const task_container = e.currentTarget.parentNode.parentNode;
+      if (e.target.checked) {
+        unfin_container.append(task_container);
+        task_container.classList.add('finished');
+        let key = task_container.getAttribute('data-key');
+        state = task_container.childNodes[0].childNodes[0].checked;
+        var task_obj = {
+          title: task_container.childNodes[0].innerText,
+          state: state,
+          key: key,
+        };
+        var updates = {};
+        updates['/tasks/' + key] = task_obj;
+        firebase.database().ref().update(updates);
+      } else {
+        unfin_container.prepend(task_container);
+        task_container.classList.remove('finished');
+        let key = task_container.getAttribute('data-key');
+        state = task_container.childNodes[0].childNodes[0].checked;
+        var task_obj = {
+          title: task_container.childNodes[0].innerText,
+          state: state,
+          key: key,
+        };
+        var updates = {};
+        updates['/tasks/' + key] = task_obj;
+        firebase.database().ref().update(updates);
+      }
+    });
+  }
+}
 
-  editButtonArray = Array.from(edit_button);
+function taskEdit(unfin_container) {
+  let edit_button = unfin_container.querySelectorAll('.task_edit_button');
+  for (let i = 0; i < edit_button.length; i++) {
+    edit_button[i].addEventListener('click', function (e) {
+      e.preventDefault();
+      e.currentTarget.style.backgroundColor = '#ffed83';
+      e.currentTarget.style.color = '#fff';
 
-  for (let i = 0; i < editButtonArray.length; i++) {
-    editButtonArray[i].addEventListener('click', function (e) {
-      finished_task_container = document.getElementById('fin-container');
-      const finishedTask = e.target.parentNode.parentNode.parentElement;
-      finishedTask.remove();
-      finished_task_container.append(finishedTask);
+      title = e.currentTarget.parentElement.previousSibling.childNodes[1];
+      title.setAttribute('contenteditable', true);
 
-      const key = finishedTask.getAttribute('data-key');
+      e.currentTarget.classList.add('editing');
+      finishEdit(unfin_container);
+    });
+  }
+}
+function finishEdit(unfin_container) {
+  let editing = unfin_container.querySelectorAll('.editing');
+  for (let i = 0; i < editing.length; i++) {
+    editing[i].addEventListener('click', function (e) {
+      e.currentTarget.classList.remove('editing');
+      e.currentTarget.style.backgroundColor = '#fff';
+      e.currentTarget.style.color = '#000';
+      editedTextNode =
+        e.currentTarget.parentElement.previousSibling.childNodes[1];
+      editedTextNode.setAttribute('contenteditable', false);
+      // Change in firebase
+      let key = editedTextNode.parentElement.parentElement.getAttribute(
+        'data-key'
+      );
       var task_obj = {
-        title: finishedTask.childNodes[0].childNodes[0].innerHTML,
+        title: editedTextNode.innerText,
+        state: false,
         key: key,
       };
-
       var updates = {};
-      updates['/finished_task/' + key] = task_obj;
+      updates['/tasks/' + key] = task_obj;
       firebase.database().ref().update(updates);
     });
   }
 }
 
-function loadFinishedTasks() {
-  const finContainer = document.getElementById('fin-container');
-  task_array = [];
-  firebase
-    .database()
-    .ref('finished_task')
-    .once('value', function (snapshot) {
-      snapshot.forEach(function (childSnapshot) {
-        var childKey = childSnapshot.key;
-        var childData = childSnapshot.val();
-        task_array.push(Object.values(childData));
-      });
-      for (var i, i = 0; i < task_array.length; i++) {
-        task_key = task_array[i][0];
-        task_title = task_array[i][1];
+function taskDelete(unfin_container) {
+  let edit_button = unfin_container.querySelectorAll('.task_delete_button');
+  for (let i = 0; i < edit_button.length; i++) {
+    edit_button[i].addEventListener('click', function (e) {
+      e.preventDefault();
+      key = e.currentTarget.parentElement.parentElement.getAttribute(
+        'data-key'
+      );
+      task_to_remove = firebase.database().ref('/tasks/' + key);
+      task_to_remove.remove();
 
-        task_container = document.createElement('div');
-        task_container.setAttribute('class', 'task_container');
-        task_container.setAttribute('data-key', task_key);
-
-        task_data = document.createElement('div');
-        task_data.setAttribute('class', 'task_data');
-
-        title = document.createElement('p');
-        title.setAttribute('class', 'task_title');
-        title.setAttribute('contenteditable', false);
-        title.innerHTML = task_title;
-
-        finContainer.append(task_container);
-        task_container.append(task_data);
-        task_data.append(title);
-      }
+      e.currentTarget.parentElement.parentElement.remove();
     });
+  }
 }
-
-function taskEdit() {}
-function taskDelete() {}
 
 let stateCheck = setInterval(() => {
   if (document.readyState === 'complete') {
     clearInterval(stateCheck);
     create_unfinished_task();
-    loadFinishedTasks();
   }
-}, 100);
+}, 10);
 
-//// Next task
-// Grab what is in fished_task on firebase and display it in the Finished task section
-// And figure out why finished tasks aren't moved out of the finished task section of firebase when the "done" button is clicked
+// let finished = unfin_container.getElementsByClassName('.finished');
+//     for (let i = 0; i < finished.length; i++) {
+//       console.log(finished[i]);
+//       finished[i].removeChild();
+//       unfin_container.append(finished[i]);
+//     }
