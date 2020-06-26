46612cee553c6e8eb250748ce7f4692ac4024266
Ben Schnell <benschnell@southern.edu>
2020-06-26 09:07:59 -0400

    complete refactor

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-// Your web app's Firebase configuration
+/*===============================================================
+Your web app's Firebase configuration
+===============================================================*/
 var firebaseConfig = {
   apiKey: 'AIzaSyAyNPBXrb1sc_TzUpIU6VCs3hGAlM9wO6A',
   authDomain: 'all-the-tools.firebaseapp.com',
@@ -10,267 +12,221 @@ var firebaseConfig = {
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
 
-// New todo list button
-const newTodoButton = document.getElementById('new-todo-button');
-let counter = 0;
-newTodoButton.addEventListener('click', function (e) {
-  counter++;
-  createTodoList(counter);
-});
+/*===============================================================
+GLOBAL STATE
+===============================================================*/
+let applicationState = {
+  listIdCounter: -1,
+  todoIdCounter: 0,
+};
 
-function createTodoList(counter) {
-  const contentContainer = document.getElementById('content_container');
-
-  // Create the elements
-  let todoList = document.createElement('div');
-  todoList.setAttribute('class', 'todo-list');
-  todoList.id = 'tdlist' + counter;
-  listId = todoList.id;
-
-  todoList.innerHTML = `
-    <div class="finish_task_header">
-      <p>To Do List</p>
-    </div>
-    <form class="add-todo-form">
-      <input class="input_box" type="text" placeholder="Task" />
-      <button class="input_button">
-        Enter<i class="fa fa-arrow-circle"></i>
-      </button>
-    </form>
-    <p class="header">Tasks</p>
-    <div class="unfin-container">
-    </div>
-    <div class="todoListDelete"><i class="fa fa-trash"></div>`;
-
-  contentContainer.append(todoList);
-
-  listenForAddTodo(todoList);
-  create_unfinished_task(listId);
+/*===============================================================
+HELPER FUNCTIONS
+===============================================================*/
+function getNextListId() {
+  applicationState.listIdCounter++;
+  return applicationState.listIdCounter;
 }
 
-function listenForAddTodo(todoList) {
-  const addTodoForm = todoList.getElementsByClassName('add-todo-form');
-  for (let i = 0; i < addTodoForm.length; i++) {
-    const box = addTodoForm[i].querySelectorAll('.input_box');
-    addTodoForm[i].addEventListener('submit', function (e) {
-      e.preventDefault();
-      addTask(todoList, box[0]);
-      box[0].value = '';
+function getNextTodoId() {
+  applicationState.todoIdCounter++;
+  return applicationState.todoIdCounter;
+}
+
+/*===============================================================
+CLICK HANDLERS
+===============================================================*/
+function handleAddListClick() {
+  // Set up variables
+  let listId = getNextListId();
+
+  // Call function to add new list
+  addList(listId);
+}
+
+function handleAddTodoClick(clickedElement) {
+  // Set up variables
+  let todoId = getNextTodoId();
+  let listId = clickedElement.getAttribute('data-list-id');
+  let todoContent = document
+    .querySelectorAll(`[data-list-id="${listId}"]`)[0]
+    .getElementsByClassName('new-todo-content-input')[0].value;
+
+  // Call function to add new todo
+  addTodo(todoId, listId, todoContent);
+
+  // return input to blank
+  clickedElement.previousSibling.value = '';
+  return false;
+}
+
+function handleRemoveListClick(clickedElement) {
+  const listId = clickedElement.parentElement.getAttribute('data-list-id');
+  removeList(clickedElement, listId);
+}
+
+function handleRemoveTodoClick(clickedElement) {
+  // Set up variables
+  let todoId = clickedElement.getAttribute('data-todo-id');
+  let listId = clickedElement.parentElement.parentElement.parentElement.parentElement.getAttribute(
+    'data-list-id'
+  );
+  removeTodo(clickedElement, todoId, listId);
+}
+
+/*===============================================================
+MAIN FUNCTIONS
+===============================================================*/
+function addList(listId) {
+  // Set up variables
+  let listsContainer = document.getElementById('lists-container');
+  let listTemplate = document.createElement('div');
+
+  // Template new list
+  listTemplate.innerHTML = `<div class="list" data-list-id="${listId}">
+      <form><input class="new-todo-content-input" type="text"></input><button onClick="return handleAddTodoClick(this)" data-list-id="${listId}">Add New Todo</button></form>
+      <h3 class="title" contenteditable="true">Title</h3>
+      <div class="todos-container">
+        <!-- Todos will go here -->
+      </div>
+      <small onclick="handleRemoveListClick(this)">X</small>
+    </div>`;
+
+  // Add new list to the DOM within lists container
+  // console.log(listId);
+  listsContainer.append(listTemplate);
+  db.ref('/lists/').set({
+    listId,
+  });
+}
+
+function addTodo(todoId, listId, todoContent) {
+  // Set up variables
+  let targetList = document.querySelectorAll(
+    `div.list[data-list-id="${listId}"]`
+  )[0];
+  let targetTodosContainer = targetList.getElementsByClassName(
+    'todos-container'
+  )[0];
+  let todoTemplate = document.createElement('div');
+
+  // Template new todo
+  todoTemplate.innerHTML = `<div class="todo" data-todo-id="${todoId}">
+      <span class="todo-text">${todoContent}</span><button onClick="handleRemoveTodoClick(this)" data-todo-id="${todoId}">Remove</button>
+    </div>`;
+  // Add new todo to the DOM within target container
+  targetTodosContainer.append(todoTemplate);
+
+  // Add todo in Firebase here
+  const key = db.ref('/lists/').child(listId).push().key;
+  var task = {
+    title: todoContent,
+    todoList: listId,
+    todoId: todoId,
+    key: key,
+  };
+
+  var updates = {};
+  updates[key] = task;
+  db.ref('lists/').child(listId).update(updates);
+
+  // Add key to data attribute on task
+  todoTemplate.querySelector('span').setAttribute('data-key', key);
+}
+
+// Remove list
+function removeList(clickedElement, listId) {
+  // Remove from Firebase
+  console.log(db.ref('/lists/').child(listId));
+  task_to_remove = db.ref('/lists/').child(listId);
+  task_to_remove.remove();
+  // Remove from DOM
+  clickedElement.parentElement.remove();
+}
+
+function removeTodo(clickedElement, todoId, listId) {
+  //Set up variables
+  let targetTodo = document.querySelectorAll(
+    `div.todo[data-todo-id="${todoId}"]`
+  )[0];
+
+  // Remove todo in Firebase here
+  const key = clickedElement.previousSibling.getAttribute('data-key');
+  task_to_remove = db.ref('/lists/').child(listId).child(key);
+  // console.log(task_to_remove);
+  task_to_remove.remove();
+
+  // Remove todo from DOM
+  targetTodo.remove();
+}
+
+// Populate to do list with existing items in database
+function loadFromDb() {
+  list_array = [];
+  db.ref('/lists/').once('value', function (snapshot) {
+    snapshot.forEach(function (childSnapshot) {
+      var childKey = childSnapshot.key;
+      var childData = childSnapshot.val();
+      list_array.push(Object.values(childData));
     });
-  }
-}
+    if (list_array != '') {
+      for (var i, i = 0; i < list_array.length; i++) {
+        // console.log(list_array[0].todoList);
+        listId = list_array[i][0].todoList;
+        // console.log(listId);
+        let listsContainer = document.getElementById('lists-container');
+        let listTemplate = document.createElement('div');
 
-function addTask(todoList, input) {
-  if (input.value.length != 0) {
-    const key = firebase.database().ref().child('unfinished_key').push().key;
-    var task = {
-      title: input.value,
-      state: false,
-      todoList: todoList.id,
-      key: key,
-    };
+        // Template new list
+        listTemplate.innerHTML = `<div class="list" id="${listId}" data-list-id="${listId}">
+          <form><input class="new-todo-content-input" type="text"></input><button onClick="return handleAddTodoClick(this)" data-list-id="${listId}">Add New Todo</button></form>
+          <h3 class="title" contenteditable="true">Title</h3>
+          <div class="todos-container">
+            
+          </div>
+          <small onclick="handleRemoveListClick(this)">X</small>
+        </div>`;
+        listsContainer.append(listTemplate);
+        newArray = list_array[i];
+        // console.log(list_array[i]);
+        // task_array = Object.values(t_array);
+        for (var j, j = 0; j < newArray.length; j++) {
+          //console.log(task_array[j]);
+          const task_array = Object.values(newArray[j]);
+          //console.log(task_array);
+          const dataKey = task_array[0];
+          // console.log(task_array[0]);
+          const list = document.getElementById(listId);
+          const todoContent = task_array[1];
+          const todoId = task_array[2];
 
-    var updates = {};
-    updates['/tasks/' + key] = task;
-    firebase.database().ref().update(updates);
-  }
-}
-
-function create_unfinished_task(listId) {
-  theList = document.getElementById(listId);
-  let unfin_container = theList.getElementsByClassName('unfin-container');
-  unfin_container = unfin_container[0];
-  console.log(unfin_container);
-  unfin_container.innerHTML = '';
-
-  task_array = [];
-  firebase
-    .database()
-    .ref('tasks')
-    .once('value', function (snapshot) {
-      snapshot.forEach(function (childSnapshot) {
-        var childKey = childSnapshot.key;
-        var childData = childSnapshot.val();
-        task_array.push(Object.values(childData));
-      });
-      for (var i, i = 0; i < task_array.length; i++) {
-        task_key = task_array[i][0];
-        state = task_array[i][1];
-        task_title = task_array[i][2];
-
-        task_container = document.createElement('div');
-        task_container.setAttribute('class', 'task_container');
-        task_container.setAttribute('data-key', task_key);
-
-        task_data = document.createElement('div');
-        task_data.setAttribute('class', 'task_data');
-
-        checkbox = document.createElement('input');
-        checkbox.setAttribute('class', 'done');
-        checkbox.type = 'checkbox';
-        checkbox.checked = state;
-
-        title = document.createElement('p');
-        title.setAttribute('class', 'task_title');
-        title.setAttribute('contenteditable', false);
-        title.innerHTML = task_title;
-
-        task_tool = document.createElement('div');
-        task_tool.setAttribute('class', 'task_tool');
-
-        task_edit_button = document.createElement('button');
-        task_edit_button.setAttribute('class', 'task_edit_button');
-        fa_edit = document.createElement('i');
-        fa_edit.setAttribute('class', 'fa fa-pencil');
-
-        task_delete_button = document.createElement('button');
-        task_delete_button.setAttribute('class', 'task_delete_button');
-        fa_delete = document.createElement('i');
-        fa_delete.setAttribute('class', 'fa fa-trash');
-
-        unfin_container.append(task_container);
-        task_container.append(task_data);
-        task_data.append(checkbox);
-        task_data.append(title);
-        task_container.append(task_tool);
-        if (state === true) {
-          task_container.classList.add('finished');
+          todoContainer = list.querySelectorAll('.todos-container')[0];
+          todoFromDb = document.createElement('div');
+          // Template new todo
+          todoFromDb.innerHTML = `<div class="todo" data-todo-id="${todoId}">
+            <span class="todo-text" data-key="${dataKey}">${todoContent}</span><button onClick="handleRemoveTodoClick(this)" data-todo-id="${todoId}">Remove</button>
+          </div>`;
+          todoContainer.append(todoFromDb);
         }
-
-        task_tool.append(task_edit_button);
-        task_edit_button.append(fa_edit);
-
-        task_tool.append(task_delete_button);
-        task_delete_button.append(fa_delete);
       }
-
-      taskDone(unfin_container);
-      taskEdit(unfin_container);
-      taskDelete(unfin_container);
-    })
-    .then(() => {
-      taskReorder(unfin_container);
-    })
-    .catch();
-}
-
-function taskReorder(unfin_container) {
-  let finished = unfin_container.getElementsByClassName('finished');
-  for (let i = 0; i < finished.length; i++) {
-    console.log(finished[i]);
-    unfin_container.append(finished[i]);
-  }
-}
-
-function taskDone(unfin_container) {
-  let done_button = unfin_container.querySelectorAll('.done');
-  for (let i = 0; i < done_button.length; i++) {
-    done_button[i].addEventListener('change', function (e) {
-      const task_container = e.currentTarget.parentNode.parentNode;
-      if (e.target.checked) {
-        unfin_container.append(task_container);
-        task_container.classList.add('finished');
-        let key = task_container.getAttribute('data-key');
-        state = task_container.childNodes[0].childNodes[0].checked;
-        var task_obj = {
-          title: task_container.childNodes[0].innerText,
-          state: state,
-          key: key,
-        };
-        var updates = {};
-        updates['/tasks/' + key] = task_obj;
-        firebase.database().ref().update(updates);
-      } else {
-        unfin_container.prepend(task_container);
-        task_container.classList.remove('finished');
-        let key = task_container.getAttribute('data-key');
-        state = task_container.childNodes[0].childNodes[0].checked;
-        var task_obj = {
-          title: task_container.childNodes[0].innerText,
-          state: state,
-          key: key,
-        };
-        var updates = {};
-        updates['/tasks/' + key] = task_obj;
-        firebase.database().ref().update(updates);
-      }
-    });
-  }
-}
-
-function taskEdit(unfin_container) {
-  let edit_button = unfin_container.querySelectorAll('.task_edit_button');
-  for (let i = 0; i < edit_button.length; i++) {
-    edit_button[i].addEventListener('click', function (e) {
-      e.preventDefault();
-      e.currentTarget.style.backgroundColor = '#ffed83';
-      e.currentTarget.style.color = '#fff';
-
-      title = e.currentTarget.parentElement.previousSibling.childNodes[1];
-      title.setAttribute('contenteditable', true);
-
-      e.currentTarget.classList.add('editing');
-      finishEdit(unfin_container);
-    });
-  }
-}
-
-function finishEdit(unfin_container) {
-  let editing = unfin_container.querySelectorAll('.editing');
-  for (let i = 0; i < editing.length; i++) {
-    editing[i].addEventListener('click', function (e) {
-      e.currentTarget.classList.remove('editing');
-      e.currentTarget.style.backgroundColor = '#fff';
-      e.currentTarget.style.color = '#000';
-      editedTextNode =
-        e.currentTarget.parentElement.previousSibling.childNodes[1];
-      editedTextNode.setAttribute('contenteditable', false);
-      // Change in firebase
-      let key = editedTextNode.parentElement.parentElement.getAttribute(
-        'data-key'
-      );
-      var task_obj = {
-        title: editedTextNode.innerText,
-        state: false,
-        key: key,
-      };
-      var updates = {};
-      updates['/tasks/' + key] = task_obj;
-      firebase.database().ref().update(updates);
-    });
-  }
-}
-
-function taskDelete(unfin_container) {
-  let edit_button = unfin_container.querySelectorAll('.task_delete_button');
-  for (let i = 0; i < edit_button.length; i++) {
-    edit_button[i].addEventListener('click', function (e) {
-      e.preventDefault();
-      key = e.currentTarget.parentElement.parentElement.getAttribute(
-        'data-key'
-      );
-      task_to_remove = firebase.database().ref('/tasks/' + key);
-      task_to_remove.remove();
-
-      e.currentTarget.parentElement.parentElement.remove();
-    });
-  }
+    }
+  });
 }
 
 let stateCheck = setInterval(() => {
   if (document.readyState === 'complete') {
     clearInterval(stateCheck);
-    createTodoList(counter);
-    create_unfinished_task(listId);
+    loadFromDb();
   }
 }, 1);
 
-// let deleteListButton = document.getElementsByClassName('todoListDelete');
-//     for (let i = 0; i < deleteListButton.length; i++) {
-//       deleteListButton[i].addEventListener('click', function (e) {
-//         e.preventDefault();
-//         console.log(e.currentTarget.parentElement);
-//         e.currentTarget.parentElement.remove();
-//       });
-//     }
+/* Next steps
+
+* Figure out how to add an editable title to the todo list so I know what it's about
+    * Maybe do this by pushing the key when the list is created, and updating the key when the todo is created. Would require a pretty decent refactor but would be worth it.
+* Make each todo item editable and save edit on enter/return key.
+* Make it so I can mark an item as done.
+
+*/
